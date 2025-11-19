36982b8c3d19c6ea36062f77fc969d1ca4a9de71
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-11-19 15:55:53 +0530

    day2

---
+++ C-lang/day2/day2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+
+  // int num = 12;     // integer
+  // float num2 = 2.6; // float
+
+  // char word = 'g'; // character
+
+  // int result = 1 + 2;
+
+  // printf("integer is %d and %d \n", num, 5);
+  // printf("result is %d  \n", result);
+  // printf("float is %0.3f", num);
+  // printf("%d",num2);
+
+  // camel case // snake case
+  // userNameInsta // user_name_insta
+
+  // + - * /  %
+
+  // int num;
+  // printf("enter the num value :  ");
+  // scanf("%d", &num); // input taking from terminal
+
+  // printf("%d", num);
+
+
+// int result = 7%5;
+// int result = 7+5;
+// int result = 7-5;
+// int result = 12/5;
+int result = 7*5;
+
+printf("%d",result);
+
+  return 0;
+}
