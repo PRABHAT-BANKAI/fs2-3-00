ed51b7823ba0d078d76f538f4e8de3946c2d26ad
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-11-20 15:40:15 +0530

    day3

---
+++ C-lang/day3/day3.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+#define PI 3.14
+int main()
+{
+  //   int num = 12;
+  //   float result = 11 + PI;
+
+  //   const int num2 = 27; // now variable  is constant
+
+  //   num = 25;
+
+  //   printf("%d\n", num);
+  //   printf("%d\n", num2);
+  //  printf("result %f\n", result);
+
+  // int num ,num2;
+  // num = 12;
+  // num2 = 13;
+
+  // int number ;
+
+  // printf("enter your number: ");
+
+  // scanf("%d",&number);
+
+  // printf("value is %d",number);
+
+  // int radius = 5;
+
+  // float areaOfCircle = PI * (radius * radius);
+
+  // printf("%f", areaOfCircle);
+
+  int eng = 15;
+  int hindi = 55;
+  int coding = 88;
+
+  int total = eng + hindi + coding;
+
+  int subject = 3;
+
+  float average = total/subject;
+
+  printf("%f",average);
+
+
+      return 0;
+}
